923cebf2d202273c4f4f8cc9ed8ae8f84f8dc3d7
Rockthor <jan.carvajal1@u.icesi.edu.co>
2024-03-27 21:45:08 -0500

    finished sign up screen design

--- app/src/main/java/com/example/ecomonitor/MainActivity.kt
+++ app/src/main/java/com/example/ecomonitor/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ecomonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecomonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
 }
