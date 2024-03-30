ea3756ff6150079169a3fa28fe17dcfec8e8a0d8
lycanfloof <lyca2206@gmail.com>
2024-03-29 22:37:55 -0500

    Implemented account creation using Firebase's authentication, added 'google-services.json' file to .gitignore

--- app/src/main/java/com/example/ecomonitor/MainActivity.kt
+++ app/src/main/java/com/example/ecomonitor/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.ecomonitor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ecomonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -13,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.toRegistrationButton.setOnClickListener { register() }
     }
 
+    private fun register() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/example/ecomonitor/SignUpActivity.kt
+++ app/src/main/java/com/example/ecomonitor/SignUpActivity.kt
@@ -2,10 +2,40 @@ package com.example.ecomonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.ecomonitor.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class SignUpActivity : AppCompatActivity() {
+    private val successMessage = "You have created your account!"
+    private val mismatchMessage = "The passwords you've entered don't match."
+
+    private val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+        binding.createAccountButton.setOnClickListener { createAccount() }
+        binding.textView10.setOnClickListener { finish() }
+    }
+
+    private fun createAccount() {
+        val email = binding.emailField.text.toString()
+        val password = binding.passwordField.text.toString()
+        val confirmPassword = binding.confirmPasswordField.text.toString()
+
+        if (password == confirmPassword) {
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener { showMessage(successMessage) ; finish() }
+                .addOnFailureListener { showMessage(it.message!!) }
+        } else { showMessage(mismatchMessage) }
+    }
+
+    private fun showMessage(text : String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 }
