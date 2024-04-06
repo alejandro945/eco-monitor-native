a65e64367ccbfa3053254b3a9275a1a6e8c5a1a4
alejandro945 <alejo8677@gmail.com>
2024-04-06 12:38:08 -0500

    feat: Adding google analytics

--- app/src/main/java/com/example/ecomonitor/MainActivity.kt
+++ app/src/main/java/com/example/ecomonitor/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.ecomonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
+        firebaseAnalytics = Firebase.analytics
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT){
+            param(FirebaseAnalytics.Param.ITEM_ID, "id");
+            param(FirebaseAnalytics.Param.ITEM_NAME, "name");
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        }
     }
+
 }
