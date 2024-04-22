97eff948e6acaa6e0cec5bcc86d265810676eea1
alejandro945 <alejo8677@gmail.com>
2024-04-22 07:59:44 -0500

    chore: Solve conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,12 +54,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.2")
-<<<<<<< HEAD
 
-=======
-    implementation("com.google.firebase:firebase-analytics:21.6.2")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
->>>>>>> 3f95625b89f562b321d411d2f425747881932d3c
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,7 +64,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
     implementation("com.google.firebase:firebase-analytics:21.6.2")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
