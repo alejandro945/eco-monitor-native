0fc9f462f5c90c854e3c8dad9e4b6cd9658429ba
Gabriel Alejandro Delgado Alvarez <1006170256@u.icesi.edu.co>
2024-06-06 17:03:00 -0500

    Merged and fixed

--- app/src/main/java/com/example/ecomonitor/presentation/view/NewDeviceActivity.kt
+++ app/src/main/java/com/example/ecomonitor/presentation/view/NewDeviceActivity.kt
@@ -1,28 +1,35 @@
 package com.example.ecomonitor.presentation.view
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ecomonitor.R
 import com.example.ecomonitor.databinding.ActivityDevicesBinding
 import com.example.ecomonitor.databinding.ActivityNewDeviceBinding
 import com.example.ecomonitor.domain.model.Device
 import com.example.ecomonitor.presentation.viewmodel.DeviceViewModel
 
 class NewDeviceActivity: AppCompatActivity()  {
-    private val binding by lazy { ActivityNewDeviceBinding.inflate(layoutInflater) }
 
     private val viewModel: DeviceViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_new_device)
 
-        binding.back.setOnClickListener { finish() }
-        binding.saveDevice.setOnClickListener {
+        val back = findViewById<Button>(R.id.back)
+        val saveDevice = findViewById<Button>(R.id.saveDevice)
+        val deviceNameET = findViewById<EditText>(R.id.deviceNameET)
+        val locationET = findViewById<EditText>(R.id.locationET)
+        back.setOnClickListener { finish() }
+        saveDevice.setOnClickListener {
             viewModel.addDevice(
                 Device(
-                    binding.deviceNameET.text.toString(),
-                    binding.locationET.text.toString(),
+                    deviceNameET.text.toString(),
+                    locationET.text.toString(),
                 )
             )
             finish()
